c6029dc92ccc99a9f28bafa803635b4a1841821b
WestleyArgentum <WestleyArgentum@gmail.com>
2015-02-04 20:45:53 -0500

    formatting, remove debug printing

--- source/visualizations/congress-is-a-game/src/opposing-groups.js
+++ source/visualizations/congress-is-a-game/src/opposing-groups.js
@@ -10,7 +10,8 @@ var x = d3.scale.linear()
     .rangeRound([0, width]);
 
 var color = d3.scale.ordinal()
-    .range(['#f6a580', '#92c6db']);
+    .range(['#f6a580', '#92c6db'])
+    .domain(['group1_percent', 'group2_percent']);
 
 var xAxis = d3.svg.axis()
     .scale(x)
@@ -24,79 +25,78 @@ var svg = d3.select('#opposing-groups').append('svg')
   .append('g')
     .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-  color.domain(['group1_percent', 'group2_percent']);
+d3.csv('data/opposing-group-stats.csv', function(error, data) {
 
-  d3.csv('data/opposing-group-stats.csv', function(error, data) {
-  data.forEach(function(d) {
-      d['group1_percent'] = +d['votes1']*100/d['totals'];
-      d['group2_percent'] = +d['votes2']*100/d['totals'];
-      var x0 = -1*(+d['group1_percent']);
+    data.forEach(function(d) {
+        d['group1_percent'] = +d['votes1']*100/d['totals'];
+        d['group2_percent'] = +d['votes2']*100/d['totals'];
+        var x0 = -1*(+d['group1_percent']);
 
-    d.boxes = color.domain().map(function(name) {
-        return {
-            name: name,
-            x0: x0,
-            x1: x0 += +d[name],
-            N: +d['totals'],
-            groupname: d[name.split('_')[0]]
-        };
+        d.boxes = color.domain().map(function(name) {
+            return {
+                name: name,
+                x0: x0,
+                x1: x0 += +d[name],
+                N: +d['totals'],
+                groupname: d[name.split('_')[0]]
+            };
+        });
     });
-  });
 
+    var min_val = d3.min(data, function(d) {
+        return d.boxes['0'].x0;
+    });
 
-  var min_val = d3.min(data, function(d) {
-          return d.boxes['0'].x0;
-          });
+    var max_val = d3.max(data, function(d) {
+        return d.boxes['1'].x1;
+    });
 
-  var max_val = d3.max(data, function(d) {
-          return d.boxes['1'].x1;
-          });
+    x.domain([min_val, max_val]).nice();
+    y.domain(data.map(function(d) { return d['group1'] + ', ' + d['group2']; }));
 
-  x.domain([min_val, max_val]).nice();
-  y.domain(data.map(function(d) { return d['group1'] + ', ' + d['group2']; }));
+    svg.append('g')
+        .attr('class', 'x axis')
+        .call(xAxis);
 
-  svg.append('g')
-      .attr('class', 'x axis')
-      .call(xAxis);
+    var vakken = svg.selectAll('.question')
+        .data(data)
+      .enter().append('g')
+        .attr('class', 'bar')
+        .attr('transform', function(d) { return 'translate(0,' + y(d['group1'] + ', ' + d['group2']) + ')'; });
 
-  var vakken = svg.selectAll('.question')
-      .data(data)
-    .enter().append('g')
-      .attr('class', 'bar')
-      .attr('transform', function(d) { return 'translate(0,' + y(d['group1'] + ', ' + d['group2']) + ')'; });
+    var bars = vakken.selectAll('rect')
+        .data(function(d) { return d.boxes; })
+      .enter().append('g')
+        .attr('class', 'subbar');
 
-  var bars = vakken.selectAll('rect')
-      .data(function(d) { return d.boxes; })
-    .enter().append('g').attr('class', 'subbar');
+    bars.append('rect')
+        .attr('height', y.rangeBand())
+        .attr('x', function(d) { return x(d.x0); })
+        .attr('width', function(d) { return x(d.x1) - x(d.x0); })
+        .style('fill', function(d) { return color(d.name); });
 
-  bars.append('rect')
-      .attr('height', y.rangeBand())
-      .attr('x', function(d) { return x(d.x0); })
-      .attr('width', function(d) { return x(d.x1) - x(d.x0); })
-      .style('fill', function(d) { return color(d.name); });
+    bars.append('text')
+        .attr('x', function(d) { return x(d.name.indexOf('group1') != -1 ? -105 : 40); })
+        .attr('y', y.rangeBand() / 2)
+        .attr('dy', '0.5em')
+        .attr('dx', '0.5em')
+        .style('font' ,'10px sans-serif')
+        .style('text-anchor', function(d) { return d.name.indexOf('group1') != -1 ? 'end' : 'begin'; })
+        .text(function(d) { return d.groupname; });
 
-  bars.append('text')
-      .attr('x', function(d) { return x(d.name.indexOf('group1') != -1 ? -105 : 40); })
-      .attr('y', y.rangeBand() / 2)
-      .attr('dy', '0.5em')
-      .attr('dx', '0.5em')
-      .style('font' ,'10px sans-serif')
-      .style('text-anchor', function(d) { console.log('TEXT', d); return d.name.indexOf('group1') != -1 ? 'end' : 'begin'; })
-      .text(function(d) { return d.groupname; });
+    vakken.insert('rect',':first-child')
+        .attr('height', y.rangeBand())
+        .attr('x', '1')
+        .attr('width', width)
+        .attr('fill-opacity', '0.5')
+        .style('fill', '#F5F5F5')
+        .attr('class', function(d,index) { return index%2==0 ? 'even' : 'uneven'; });
 
-  vakken.insert('rect',':first-child')
-      .attr('height', y.rangeBand())
-      .attr('x', '1')
-      .attr('width', width)
-      .attr('fill-opacity', '0.5')
-      .style('fill', '#F5F5F5')
-      .attr('class', function(d,index) { return index%2==0 ? 'even' : 'uneven'; });
-
-  svg.append('g')
-      .attr('class', 'y axis')
-  .append('line')
-      .attr('x1', x(0))
-      .attr('x2', x(0))
-      .attr('y2', height);
+    svg.append('g')
+        .attr('class', 'y axis')
+      .append('line')
+        .attr('x1', x(0))
+        .attr('x2', x(0))
+        .attr('y2', height);
 
 });
